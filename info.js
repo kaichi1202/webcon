711b188e39399c6f466ed7d6b600eedba8873cdb
kaichi1202 <kaichikoro@icloud.com>
2017-07-18 22:41:34 +0900

    info.jsを整理

--- info.js
+++ info.js
@@ -1,58 +1,57 @@
 var isFirst = true;
 
 function changeIe(){
-    if (isFirst == false){
-        $(".ie, .me, .sa, .gc").css("transition", "1s");//  1秒かけて変わるようにする
-                                                        //  ページ幅変更時に影響を受けないように設定
-    }
-    $(".me, .sa, .gc").css("opacity", "0");             //  他の要素を隠す
-    $(".meb, .sab, .gcb").css("opacity", "0.5");        //  他のボタンを薄くする
-    $(".ieb").css("opacity", "1");                      //  ボタンを透明でないようにする
-    $(".ie").css("opacity", "1");                       //  この要素を出現させる
-    $(".ie").attr("class", "ie active");                //  アクティブにする
+	if (isFirst == false){
+		$(".ie, .me, .sa, .gc").css("transition", "1s");//  1秒かけて変わるようにする
+	}
+	$(".me, .sa, .gc").css("opacity", "0");             //  他の要素を隠す
+	$(".meb, .sab, .gcb").css("opacity", "0.5");        //  他のボタンを薄くする
+	$(".ieb").css("opacity", "1");                      //  ボタンを透明でないようにする
+	$(".ie").css("opacity", "1");                       //  この要素を出現させる
+	$(".ie").attr("class", "ie active");                //  アクティブにする
 }
 
 function changeMe(){
-    $(".ie, .me, .sa, .gc").css("transition", "1s");
-    $(".ie, .sa, .gc").css("opacity", "0");
-    $(".ieb, .sab, .gcb").css("opacity", "0.5");
-    $(".meb").css("opacity", "1");
-    $(".me").css("opacity", "1");
-    $(".me").attr("class", "me active");
+	$(".ie, .me, .sa, .gc").css("transition", "1s");
+	$(".ie, .sa, .gc").css("opacity", "0");
+	$(".ieb, .sab, .gcb").css("opacity", "0.5");
+	$(".meb").css("opacity", "1");
+	$(".me").css("opacity", "1");
+	$(".me").attr("class", "me active");
 }
 
 function changeSa(){
-    $(".ie, .me, .sa, .gc").css("transition", "1s");
-    $(".ie, .me, .gc").css("opacity", "0");
-    $(".ieb, .meb, .gcb").css("opacity", "0.5");
-    $(".sab").css("opacity", "1");
-    $(".sa").css("opacity", "1");
-    $(".sa").attr("class", "sa active");
+	$(".ie, .me, .sa, .gc").css("transition", "1s");
+	$(".ie, .me, .gc").css("opacity", "0");
+	$(".ieb, .meb, .gcb").css("opacity", "0.5");
+	$(".sab").css("opacity", "1");
+	$(".sa").css("opacity", "1");
+	$(".sa").attr("class", "sa active");
 }
 
 function changeGc(){
-    $(".ie, .me, .sa, .gc").css("transition", "1s");
-    $(".ie, .me, .sa").css("opacity", "0");
-    $(".ieb, .meb, .sab").css("opacity", "0.5");
-    $(".gcb").css("opacity", "1");
-    $(".gc").css("opacity", "1");
-    $(".gc").attr("class", "gc active");
+	$(".ie, .me, .sa, .gc").css("transition", "1s");
+	$(".ie, .me, .sa").css("opacity", "0");
+	$(".ieb, .meb, .sab").css("opacity", "0.5");
+	$(".gcb").css("opacity", "1");
+	$(".gc").css("opacity", "1");
+	$(".gc").attr("class", "gc active");
 }
 
 function ieClick(){
-    changeIe();
+	changeIe();
 }
 
 function meClick(){
-    changeMe();
+	changeMe();
 }
 
 function saClick(){
-    changeSa();
+	changeSa();
 }
 
 function gcClick(){
-    changeGc();
+	changeGc();
 }
 
 window.onload = function(){
@@ -64,12 +63,12 @@ $(window).resize(function(){
 });
 
 function setInfoPosition(){
-    $(".ie, .me, .sa, .gc").css("transition", "0s");
-    var oneImgWidth = $(".active").width();
+	$(".ie, .me, .sa, .gc").css("transition", "0s");
+	var oneImgWidth = $(".active").width();
 	$(".ie, .me, .sa, .gc").css("height", oneImgWidth * (844 / 2880));
 	$(".systems").css("height", oneImgWidth * (844 / 2880));
 	if (isFirst == true){
-	    changeIe();
-	    isFirst = false;
+		changeIe();
+		isFirst = false;
 	}
 }
